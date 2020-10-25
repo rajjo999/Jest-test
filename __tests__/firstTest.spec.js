8f1495d1ca217c83b5ff14938e8b8957e8257efc
Ranbir singh <ranbir.singh@samsung.com>
2020-10-25 02:31:26 -0400

    Snapshot testing

--- __tests__/firstTest.spec.js
+++ __tests__/firstTest.spec.js
@@ -4,7 +4,7 @@ function filterByTerm(inputArr, searchTerm) {
   });
 }
 describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
+  it("it should filter by a search term (link)", () => {
     const input = [
       { id: 1, url: "https://www.url1.dev" },
       { id: 2, url: "https://www.url2.dev" },
@@ -16,4 +16,46 @@ describe("Filter function", () => {
     expect(filterByTerm(input, "link")).toEqual(output);
 
   });
+
+  it('will check the matchers and pass', () => {
+    const user = {
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+      name: 'LeBron James',
+    };
+  
+    expect(user).toMatchSnapshot({
+      createdAt: expect.any(Date),
+      id: expect.any(Number),
+    });
+  });
+  
+  // Snapshot
+  exports[`will check the matchers and pass 1`] = `
+  Object {
+    "createdAt": Any<Date>,
+    "id": Any<Number>,
+    "name": "LeBron James",
+  }
+  `;
+
+  it('will check the values and pass', () => {
+    const user = {
+      createdAt: new Date(),
+      name: 'Bond... James Bond',
+    };
+  
+    expect(user).toMatchSnapshot({
+      createdAt: expect.any(Date),
+      name: 'Bond... James Bond',
+    });
+  });
+  
+  // Snapshot
+  exports[`will check the values and pass 1`] = `
+  Object {
+    "createdAt": Any<Date>,
+    "name": 'Bond... James Bond',
+  }
+  `;
 });
